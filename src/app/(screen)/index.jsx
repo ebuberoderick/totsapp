3f68390aaf2d3e0a54453e3a7cca23889f7ae305
ebuberoderick <ebuberoderick2@gmail.com>
2024-11-14 08:12:34 +0000

    added home page preloader

--- src/app/(screen)/index.jsx
+++ src/app/(screen)/index.jsx
@@ -1,6 +1,8 @@
 import { useRouter } from "expo-router";
-import React from "react";
-import { TouchableOpacity } from "react-native";
+import React, { useEffect, useState } from "react";
+import { ScrollView, TouchableOpacity } from "react-native";
+import AntDesign from "react-native-vector-icons/AntDesign"
+import Ionicons from "react-native-vector-icons/Ionicons"
 import { Text, View } from "react-native";
 import { SignOut } from "../../hooks/Auth";
 import { useDispatch } from "react-redux";
@@ -8,12 +10,30 @@ import AppLayout from "../../components/layout/AppLayout";
 import HomePreloader from "../../components/perloader/HomePreloader";
 
 export default function Page() {
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false)
+    }, 4000);
+  }, [])
+
   return (
     <AppLayout>
-      <Content />
-      {/* <ScrollView className="px-3" indicatorStyle="white">
-        <HomePreloader />
-      </ScrollView> */}
+      <ScrollView className=" " indicatorStyle="white">
+        <View className="h-14 items-center px-3 gap-3 flex-row sticky top-0">
+          <View className="flex-grow"></View>
+          <View><AntDesign name="search1" size={25} /></View>
+          <TouchableOpacity className="relative">
+            <Ionicons name="notifications-outline" size={25} />
+            <View className="bg-danger rounded-full h-2 w-2 absolute right-[4px] top-1" />
+          </TouchableOpacity>
+        </View>
+        <View className="px-3 mt-4 pb-32">
+          {loading && <HomePreloader />}
+          {!loading && <Content />}
+        </View>
+      </ScrollView>
     </AppLayout>
   );
 }
